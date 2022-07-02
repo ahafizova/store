a13896eb3c893d9d077c6ce1df575360a393263b
ahafizova <64238717+ahafizova@users.noreply.github.com>
2022-07-02 18:05:58 +0500

    Add files via upload

--- dip.py
+++ dip.py
@@ -5,6 +5,7 @@ a_h = ssdeep.hash_from_file(a)
 
 b = '/home/user/a07.apk'
 b_h = ssdeep.hash_from_file(b)
+b_h = '393216:H0MN1xjIAbL4d73dW9HL3cd03LCd2jkMRjC4FbKLm9xjXZfL7:HxSAbL4d5qHLMd03OdZCC40Cpf/'
 
 res = ssdeep.compare(a_h, b_h)
 print(a_h)
@@ -12,11 +13,23 @@ print(b_h)
 print(res)
 
 """
+base:
 a070fcf0b20e1bf5e7572d3c2eae1c68d8efe019b632a539d8f86aa333c36b9f.zip
+
+new:
 6c8de4a7836adb66fb878b1fba4f27bad96f9d4824a128c303c987569a54f9f4.zip
 
 196608:bveWhAO4OFcitjKqtfd+r/bo1E8lBbQcqp5inUFe9yWUKjrA6L/XUMXcuX8AKrRE:FAYKmoo1h3Cp5iUF1vkr1/XRFmQ/
 196608:3veWhAO4OFcitjKqtfd+r/bo1E8lBbQcqp5inUFe9yWUKjrJ6JXUHXcuX8AKrRG7:5AYKmoo1h3Cp5iUF1vkr6XsFmE
 91
+
+
+75f9f66bcbfb732b3720f9b56a5b56a3a671ff4386b0b7dc8195124b0db206c0
+98304:Vz2v/oOd4+e/h8WBJbiBUUPBnzvU0C/MHpCNnvB:koJ+e/h8WfmBxVRqMJC/
+
+2192345562b9fb1536465d31f465015cb8b0a029fa118166de0d6975bd00448b
+393216:H0MN1xjIAbL4d73dW9HL3cd03LCd2jkMRjC4FbKLm9xjXZfL7:HxSAbL4d5qHLMd03OdZCC40Cpf/
+
+
 """
 
